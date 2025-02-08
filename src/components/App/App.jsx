05dac1a5f18381dbf0f2ea6396fb5265c3bc2d3c
Letice Zwinger <leticedss@gmail.com>
2025-02-08 12:55:36 -0600

    add tittle, subtittle and classes to Header

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import Header from "../Header/Header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <h1>News Explorer</h1>
+    </div>
+  );
+}
+
+export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,18 @@
-import "Header.css";
-
+import "./Header.css";
 function Header() {
-  return <header className="header"> Is this working?</header>;
+  return (
+    <header className="header" id="header">
+      <div className="header-field">
+        <div className="header__container">
+          <h1 className="header__title">What's going on in the world?</h1>
+          <p className="header__subtitle">
+            Find the latest news on any topic and save them in your personal
+            account.
+          </p>
+        </div>
+      </div>
+    </header>
+  );
 }
+
 export default Header;
