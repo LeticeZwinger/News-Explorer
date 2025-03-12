bc06fe18777c2111b2e880a99c61e89b372c7691
Letice Zwinger <leticedss@gmail.com>
2025-03-12 17:05:33 -0500

    base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/News-Explorer/", // Replace "my-app" with your repository name
+  base: "/src/",
 });
