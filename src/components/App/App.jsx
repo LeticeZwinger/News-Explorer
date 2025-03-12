53ad17834da749dfa21d5c1056acdf0481bb1a5d
Letice Zwinger <leticedss@gmail.com>
2025-03-12 16:44:40 -0500

    fix router on app

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { useAuth } from "../../Context/AuthContext";
 import SavedArticles from "../SavedArticles/SavedArticles";
 import Header from "../Header/Header";
