5f354ad83cc27cadbbff2ab8aef2fdaaeade9dc2
Letice Zwinger <leticedss@gmail.com>
2025-03-23 14:55:08 -0500

    creat structure of styles for learn more page

--- src/components/LearnMore/LearnMore.jsx
+++ src/components/LearnMore/LearnMore.jsx
@@ -9,8 +9,8 @@ function LearnMore() {
       <p className="learn-more__paragraph">detailed professional xp</p>
       <h2 className="learn-more__subtitle">education</h2>
       <p className="learn-more__paragraph">detailed education</p>
-      <ul className="learn-more__skills">
-        List of skills
+      <ul className="learn-more__skill">
+        <h2 className="learn-more__subtitle">List of skills</h2>
         <li className="learn-more__skill-item">JavaScript</li>
         <p className="learn-more__skill-description"> JavaScript description</p>
         <li className="learn-more__skill-item">React</li>
