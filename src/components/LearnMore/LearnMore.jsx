7dee45c8492ee3fa42cfeeece5ebf6f847f084a3
Letice Zwinger <leticedss@gmail.com>
2025-03-23 14:07:00 -0500

    style temporary msg for learn more page

--- src/components/LearnMore/LearnMore.jsx
+++ src/components/LearnMore/LearnMore.jsx
@@ -1,5 +1,13 @@
+import "./LearnMore.css";
 function LearnMore() {
-  return <div>Hello there!</div>;
+  return (
+    <div className="learn-more">
+      Hello there!
+      <p className="learn-more_temporary-msg">
+        This page is still being worked on ⚙️
+      </p>
+    </div>
+  );
 }
 
 export default LearnMore;
