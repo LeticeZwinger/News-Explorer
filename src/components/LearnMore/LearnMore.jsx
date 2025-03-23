30abfa670adb0076db966d86dbeb673bf8024ef0
Letice Zwinger <leticedss@gmail.com>
2025-03-23 14:48:08 -0500

    create structure of learn more page

--- src/components/LearnMore/LearnMore.jsx
+++ src/components/LearnMore/LearnMore.jsx
@@ -2,10 +2,28 @@ import "./LearnMore.css";
 function LearnMore() {
   return (
     <div className="learn-more">
-      Hello there!
-      <p className="learn-more_temporary-msg">
-        This page is still being worked on ⚙️
-      </p>
+      <h1 className="learn-more__title">About my history</h1>
+      <h2 className="learn-more__subtitle">professonal sumary</h2>
+      <p className="learn-more__paragraph">detailed professional</p>
+      <h2 className="learn-more__subtitle">professional experience</h2>
+      <p className="learn-more__paragraph">detailed professional xp</p>
+      <h2 className="learn-more__subtitle">education</h2>
+      <p className="learn-more__paragraph">detailed education</p>
+      <ul className="learn-more__skills">
+        List of skills
+        <li className="learn-more__skill-item">JavaScript</li>
+        <p className="learn-more__skill-description"> JavaScript description</p>
+        <li className="learn-more__skill-item">React</li>
+        <p className="learn-more__skill-description"> React description</p>
+        <li className="learn-more__skill-item">Node.js</li>
+        <p className="learn-more__skill-description"> Node.js description</p>
+        <li className="learn-more__skill-item">Figma design</li>
+        <p className="learn-more__skill-description">
+          Figma design description
+        </p>
+        <li className="learn-more__skill-item">REST api</li>
+        <p className="learn-more__skill-description"> REST api description</p>
+      </ul>
     </div>
   );
 }
