9bc5cd105bce4fdbee8157f9127a015e43906f71
Letice Zwinger <leticedss@gmail.com>
2025-04-02 17:57:48 -0500

    create cards of skills

--- src/components/LearnMore/LearnMore.jsx
+++ src/components/LearnMore/LearnMore.jsx
@@ -1,31 +1,56 @@
 import "./LearnMore.css";
 function LearnMore() {
   return (
-    <div className="learn-more">
-      <h1 className="learn-more__title">About my history</h1>
-      <p>My history</p>
-      <h2 className="learn-more__subtitle">professonal sumary</h2>
-      <p className="learn-more__paragraph">detailed professional</p>
-      <h2 className="learn-more__subtitle">professional experience</h2>
-      <p className="learn-more__paragraph">detailed professional xp</p>
-      <h2 className="learn-more__subtitle">education</h2>
-      <p className="learn-more__paragraph">detailed education</p>
-      <ul className="learn-more__skill">
-        <h2 className="learn-more__subtitle">List of skills</h2>
-        <li className="learn-more__skill-item">JavaScript</li>
-        <p className="learn-more__skill-description"> JavaScript description</p>
-        <li className="learn-more__skill-item">React</li>
-        <p className="learn-more__skill-description"> React description</p>
-        <li className="learn-more__skill-item">Node.js</li>
-        <p className="learn-more__skill-description"> Node.js description</p>
-        <li className="learn-more__skill-item">Figma design</li>
-        <p className="learn-more__skill-description">
-          Figma design description
-        </p>
-        <li className="learn-more__skill-item">REST api</li>
-        <p className="learn-more__skill-description"> REST api description</p>
-      </ul>
-    </div>
+    <section className="portfolio">
+      <div className="portifolio__skills">
+        <div className="portfolio__intro">
+          <h1 className="portfolio__title">Hi, I’m Letice 👩🏻‍💻</h1>
+          <p className="portfolio__description">
+            A web developer passionate about building intuitive, user-friendly
+            digital experiences. Here’s a glimpse into what I do best.
+          </p>
+        </div>
+        <div className="portfolio__skills-grid">
+          <article className="portfolio__skill-card">
+            <h2 className="portfolio__skill-title">JavaScript</h2>
+            <p className="portfolio__skill-desc">
+              I use JS to build interactive web applications and logic-heavy
+              components in React projects.
+            </p>
+          </article>
+
+          <article className="portfolio__skill-card">
+            <h2 className="portfolio__skill-title">Node.js</h2>
+            <p className="portfolio__skill-desc">
+              I built backend APIs and server-side logic using Express and
+              MongoDB for clean, scalable apps.
+            </p>
+          </article>
+
+          <article className="portfolio__skill-card">
+            <h2 className="portfolio__skill-title">Node.js</h2>
+            <p className="portfolio__skill-desc">
+              I built backend APIs and server-side logic using Express and
+              MongoDB for clean, scalable apps.
+            </p>
+          </article>
+          <article className="portfolio__skill-card">
+            <h2 className="portfolio__skill-title">Node.js</h2>
+            <p className="portfolio__skill-desc">
+              I built backend APIs and server-side logic using Express and
+              MongoDB for clean, scalable apps.
+            </p>
+          </article>
+          <article className="portfolio__skill-card">
+            <h2 className="portfolio__skill-title">Node.js</h2>
+            <p className="portfolio__skill-desc">
+              I built backend APIs and server-side logic using Express and
+              MongoDB for clean, scalable apps.
+            </p>
+          </article>
+        </div>
+      </div>
+    </section>
   );
 }
 
